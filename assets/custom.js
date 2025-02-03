e7e90b170e81961f8a2c5dff46e717a985bac807
Sheldon <sheldon@okgeek.co.za>
2025-02-03 10:05:08 +0200

    Update files

--- assets/custom.js
+++ assets/custom.js
@@ -1,5 +1,44 @@
-let burgerIcon = document.querySelector(".burger-icon");
+function initializeBurgerNav(scope = document) {
+  const burgerNav = scope.querySelector(".nav-burger");
 
-burgerIcon.addEventListener("click", () => {
-    burgerIcon.classList.toggle("open");
+  if (burgerNav) {
+    burgerNav.addEventListener("click", () => {
+      const isOpen = burgerNav.classList.toggle("open");
+      document.body.style.overflow = isOpen ? "hidden" : "";
+    });
+  }
+}
+
+function initializeMenuAccordion(scope = document) {
+  const menuAccordionParent = document.querySelectorAll(".burger .parent-link");  
+
+  menuAccordionParent.forEach(menuParent => {
+    menuParent.addEventListener("click", event => {
+      /* Only one open at a time */
+      const openParentMenu = document.querySelector(".burger .parent-link.open");
+      if(openParentMenu && openParentMenu !== menuParent) {
+        openParentMenu.classList.toggle("open");
+        openParentMenu.nextElementSibling.style.maxHeight = 0;
+      }
+      /* Open and close */
+      menuParent.classList.toggle("open");
+      
+      const menuChild = menuParent.nextElementSibling;
+      if(menuParent.classList.contains("open")) {
+        menuChild.style.maxHeight = menuChild.scrollHeight + "px";
+      } else {
+        menuChild.style.maxHeight = 0;
+      }
+    });
+  });
+}
+
+initializeBurgerNav();
+initializeMenuAccordion();
+
+
+document.addEventListener("shopify:section:load", (event) => {
+  initializeBurgerNav(event.target);
+  initializeMenuAccordion(event.target);
 });
+
